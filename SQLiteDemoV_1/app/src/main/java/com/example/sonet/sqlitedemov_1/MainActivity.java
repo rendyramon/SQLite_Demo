2bfa5770575ac14178f8fdd6121481dfa3a2ab78
Khan Sonet <khansonet@yahoo.com>
2016-07-26 00:46:54 +0600

    InsertQuery added

--- SQLiteDemoV_1/app/src/main/java/com/example/sonet/sqlitedemov_1/MainActivity.java
+++ SQLiteDemoV_1/app/src/main/java/com/example/sonet/sqlitedemov_1/MainActivity.java
@@ -3,24 +3,43 @@ package com.example.sonet.sqlitedemov_1;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
 
-    DataBaseHelper dataBaseHelper;
+    DataBaseAdapter dataBaseAdapter;
     SQLiteDatabase sqLiteDatabase;
+
+    EditText studentName;
+    EditText studentPassword;
+    Button clickBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
+        studentName = (EditText) findViewById(R.id.studentName);
+        studentPassword = (EditText) findViewById(R.id.studentPassword);
+        clickBtn = (Button) findViewById(R.id.clickBtn);
+        dataBaseAdapter = new DataBaseAdapter(this);
+    }
 
-        dataBaseHelper = new DataBaseHelper(this);
-        sqLiteDatabase = dataBaseHelper.getWritableDatabase();
-        textView.setText(dataBaseHelper.error);
+    public void click(View view) {
 
+        String name = studentName.getText().toString();
+        String password = studentPassword.getText().toString();
 
+       long id = dataBaseAdapter.queryInsert(name,password);
+
+        if(id<0)
+        {
+            Massage.getMassage(this,"All is wrong");
+        }
+        else
+        {
+            Massage.getMassage(this,"Inserted Successfully");
+        }
     }
 }
